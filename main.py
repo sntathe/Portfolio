2c85f2be78ea065829c8026c42fe05432026c418
sntathe <sntathe@gmail.com>
2024-01-04 00:03:11 +0530

    resize photo

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.JPG", width=400)
+    st.image("images/photo.JPG")
 
 with col2:
     st.title("Saket Tathe")
@@ -15,3 +15,5 @@ with col2:
     This is my portfolio app
     """
     st.info(content)
+
+st.write("Below are the App I have developed in Python. Feel free to contact me!")
