d3ca2c653cbb63b4050d52c0e9536fdcfced1edf
sntathe <sntathe@gmail.com>
2024-01-03 23:48:35 +0530

    Add my photo and code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.set_page_config(layout="wide")
 col1 , col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.png")
+    st.image("images/photo.JPG")
 
 with col2:
     st.title("Saket Tathe")
