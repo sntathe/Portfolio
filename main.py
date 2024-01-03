cedbe46ddf869b062ed805b2160db41e48e67648
sntathe <sntathe@gmail.com>
2024-01-03 23:54:39 +0530

    Add my photo and code

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 
 st.set_page_config(layout="wide")
 
-col1 , col2 = st.columns(2)
+col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.JPG")
+    st.image("images/photo.JPG", width=400)
 
 with col2:
     st.title("Saket Tathe")
