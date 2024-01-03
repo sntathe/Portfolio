0c62c8fb1ed07eaf6107fb706cfd29557252a623
sntathe <sntathe@gmail.com>
2024-01-04 00:20:04 +0530

    Render app titles

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
 with col1:
-    st.image("images/photo.JPG")
+    st.image("images/passport.JPG")
 
 with col2:
     st.title("Saket Tathe")
@@ -17,3 +18,15 @@ with col2:
     st.info(content)
 
 st.write("Below are the App I have developed in Python. Feel free to contact me!")
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
