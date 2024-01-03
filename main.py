45a39d33875fb5ef60f56d62e23a3662ecd91009
sntathe <sntathe@gmail.com>
2024-01-03 23:47:00 +0530

    Add my photo and code

---
+++ main.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1 , col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Saket Tathe")
+    content = """
+    Hi , I am Saket Tathe 
+    I am trying to do python programming.
+    This is my portfolio app
+    """
+    st.info(content)
